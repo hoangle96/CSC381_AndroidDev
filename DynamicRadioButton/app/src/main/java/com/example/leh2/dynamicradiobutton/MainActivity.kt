3fad8f0da3ea97147b62285babd8247a3af748b0
hoangle96 <lebahoang010296@gmail.com>
2018-02-21 23:46:20 -0500

    Fixing Dynamic RadioButton lab
    Using the CookingSteak lab to fix the RadioButton lab

--- DynamicRadioButton/app/src/main/java/com/example/leh2/dynamicradiobutton/MainActivity.kt
+++ DynamicRadioButton/app/src/main/java/com/example/leh2/dynamicradiobutton/MainActivity.kt
@@ -11,50 +11,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        createDonenessChoices()
         createSizeChoices()
+        createDonenessChoices()
         onclickButton()
-
-
     }
 
     private fun onclickButton() {
-        val radioDonenessGroup = findViewById<RadioGroup>(R.id.radGrpDone)
-        val radioSizeGroup = findViewById<RadioGroup>(R.id.radGrpSize)
-
         var size = 0.0
-        var res: Double
+        var res = 0.0
 
         val btnCalc = findViewById<Button>(R.id.btnCalc)
         val radGrpDone = findViewById<RadioGroup>(R.id.radGrpDone)
         val radGrpSize = findViewById<RadioGroup>(R.id.radGrpSize)
         val tvRes = findViewById<TextView>(R.id.tvRes)
 
-        val radioDone = radGrpDone.checkedRadioButtonId
-        val radioSize = radGrpSize.checkedRadioButtonId
-        Toast.makeText(applicationContext, radioDone.toString() , LENGTH_LONG).show()
-
-//        if(radioDone==-1){
-//            //if no radio button selected
-//            Toast.makeText(applicationContext, "Please select a doneness level" , LENGTH_LONG).show()
-//        }
-//
-//        if(radioSize==-1){
-//            //if no radio button selected
-//            Toast.makeText(applicationContext, "Please select a size" , LENGTH_LONG).show()
-//        }
-
         btnCalc.setOnClickListener({
 
-            when (radioSize) {
-                0 -> size = 1.0
-                1 -> size = 1.5
-                2 -> size = 2.0
-                3 -> size = 2.5
-                4 -> size = 2.0
+            val radioDone = radGrpDone.checkedRadioButtonId
+
+
+            val radioSize = radGrpSize.checkedRadioButtonId
+
+            val radioButtonSize = findViewById<RadioButton>(radioSize)
+
+            size = radioButtonSize.text.toString().toDouble()
+
+            val donenessIndex = radioDone - 6
+
+            res = 2 + donenessIndex + size * 2
+            if(radioDone==-1){
+                Toast.makeText(applicationContext, "Please select a doneness level" , LENGTH_LONG).show()
+                res = 0.0
             }
-            // Toast.makeText(applicationContext, size.toString() , LENGTH_LONG).show()
-            res = 2 + radioDone + size * 2
+
+            if(radioSize==-1){
+                //if no radio button selected
+                Toast.makeText(applicationContext, "Please select a size" , LENGTH_LONG).show()
+                res = 0.0
+            }
             tvRes.text = res.toString()
         })
 
